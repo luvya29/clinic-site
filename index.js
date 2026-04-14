49188056c012354ba625ac11f1e6b8eec0fa4ef6
luvya29 <138918172+luvya29@users.noreply.github.com>
2026-04-14 23:34:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
+// Serve static files (your HTML, CSS, images)
+app.use(express.static(__dirname));
+
+// Route for homepage
 app.get("/", (req, res) => {
-  res.send("Clinic server running");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => console.log("Server started"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
